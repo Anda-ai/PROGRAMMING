afcb3e5271c29a8ede5bed567ac1538c51305f6e
Mxoza <Mxozai@github.com>
2021-10-04 13:19:13 +0200

    First lesson commits

---
+++ EXAMPLES/EXAMPLE 1 inheritance with static variables/ClassA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author anda
+ */
+/*
+This class is used by class C for demostration of abstract class
+*
+*/
+public class ClassA {
+    static int a = 5;
+    static int b = 10;
+}
---
+++ EXAMPLES/EXAMPLE 2 inheritance with non-static variables and abstract class/ClassA.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author anda
+ */
+public class ClassA {
+    int a = 5;
+    int b = 10;
+    public ClassA(int c){
+        System.out.println(c+" + "+c+" = "+(c+c));
+    }
+}
